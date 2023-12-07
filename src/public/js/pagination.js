122d2056d8863d3492bda6aa612a1fb5db3c2f00
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-06 20:25:17 -0600

    instalando dataTables y paginando las tablas

---
+++ src/public/js/pagination.js
@@ -0,0 +1,27 @@
+  $(document).ready(function() {
+    $('#myTable').DataTable({
+      language: {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros por página",
+        "sZeroRecords": "No se encontraron resultados",
+        "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",
+        "sInfoFiltered": "(filtrado de _MAX_ registros en total)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+          "sFirst": "Primero",
+          "sLast": "Último",
+          "sNext": "Siguiente",
+          "sPrevious": "Anterior"
+        },
+        "oAria": {
+          "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+      }
+    });
+  });
