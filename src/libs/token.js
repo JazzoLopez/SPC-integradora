8c1f17232ad7047693614959ba226719534814fc
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-06 19:33:00 -0600

    Agregando las funciones del emmail, token y el mapa

---
+++ src/libs/token.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { v4 } from "uuid";
+import { UUID } from "sequelize";
+dotenv.config({
+    path: 'src/.env'
+});
+
+console.log(UUID)
+
+// Token propio
+const generateToken = () => Math.random().toString(32).substring(3) + Date.now().toString(32) + Math.random().toString(32).substring(3); //* numero aleatorio, fecha, numero aleatorio
+//  jwtoken
+const generateJwt = (userID) =>
+    jwt.sign({
+        domain: process.env.JWT_DOMAIN,
+        signature: process.env.JWT_SIGNATURE,
+        author: process.env.JWT_AUTHOR,
+        year: process.env.JWT_YEAR,
+        userID,
+    }, process.env.JWT_SECRET_HASH_STRING, {
+        expiresIn: '1d'
+    })
+
+
+
+    const decodeJwt = (token) => {
+        try {
+            return jwt.verify(token, process.env.JWT_SECRET_HASH_STRING); // Reemplaza 'tu_secreto' con tu clave secreta
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    };
+
+//module.exports(generateToken);
+export {
+    generateToken, generateJwt, decodeJwt
+}
