0b8cd14ee921b87a0488b0230a77427b5d0f82ad
juan45678 <144082821+juan45678@users.noreply.github.com>
2023-12-10 21:34:30 -0600

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -14,7 +14,7 @@ const insertUser = async (req, res) => {
     await check("name").notEmpty().withMessage("El nombre es obligatorii").run(req) //* Express checa el nombre que no venga vacio AHORA MISMO
     await check("lastname").notEmpty().withMessage("Los apellidos son obligatorios").run(req) //* Express checa el nombre que no venga vacio AHORA MISMO
     await check("tel").notEmpty().withMessage("El numero telefonico es obligatorio").run(req) //* Express checa el nombre que no venga vacio AHORA MISMO
-    await check("email").notEmpty().withMessage("El correo es obligatorio").isEmail().withMessage("Ese no es un formato valido").run(req) // Express checa que el email no venga vacio
+    await check("email").notEmpty().withMessage("El correo es obligatorio").isEmail().withMessage("Ese no es un formato valido").run(req) // Express checa que el email no venga vacio y que el formato sea el correcto
     await check("password").notEmpty().withMessage("La contraseña es obligatoria").isLength({ min: 8, max: 20 }).withMessage("La contraseña debe de ser de almenos 8 caracteres").run(req)
     await check("repeatPassword").notEmpty().withMessage("La contraseña es obligatoria").isLength({ min: 8, max: 20 }).withMessage("La contraseña debe de ser de almenos 8 caracteres").equals(req.body.password).withMessage("Las contraseñas no son iguales").run(req)
     //res.json(validationResult(req));
