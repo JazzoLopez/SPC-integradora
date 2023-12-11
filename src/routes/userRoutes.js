a3bff32f80e5fe82d6622c7672ec6d467352a917
LuisitoGamer69 <luisivmaraz03@gmail.com>
2023-12-10 23:27:17 -0600

    Actualizando configuraciones de userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,29 +1,42 @@
+// Importación del módulo 'express' y de funciones específicas desde el controlador
 import express from "express";
-import { index, formLogin, formPasswordRecovery, userHome, authenticateUser, logout, confirmAccount, formPasswordUpdate, emailChangePassword, updatePassword} from "../controllers/userController.js";
-import { adminHome } from "../controllers/adminController.js";
+import {
+    index,
+    formLogin,
+    formPasswordRecovery,
+    userHome,
+    authenticateUser,
+    logout,
+    confirmAccount,
+    formPasswordUpdate,
+    emailChangePassword,
+    updatePassword
+} from "../controllers/userController.js";
+import {
+    adminHome
+} from "../controllers/adminController.js";
+
+// Creación del enrutador utilizando express.Router()
 const router = express.Router();
 
-//* PARA PRUEBAS:
+//* PARA PRUEBAS: Redirección de la raíz a '/inicio'
 router.get('/', (req, res) => {
-    res.redirect('/inicio')
-})
+    res.redirect('/inicio');
+});
 
+// Definición de rutas para peticiones GET
+router.get('/inicio', index); //  LA PAGINA GENERAL
+router.get('/login', formLogin); //  EL FORMULARIO DE LOGIN
+router.get('/home', userHome); //  EL INICIO DE USUARIOS
+router.get('/admin-home', adminHome); //  EL INICIO DE ADMIN JUNTO AL DASHBOARD
+router.get('/logout', logout); // CIERRA SESIÓN 
+router.get('/login/confirmar-cuenta/:token', confirmAccount); // CONFIRMA LA CUENTA
+router.get('/login/cambiar-contrase%C3%B1a', formPasswordRecovery);  // RESTABLECE COONTRASENIA
+router.get('/login/actualizar-contrase%C3%B1a/:token', formPasswordUpdate);  // ACTUALIZA CONTRASENIA
 
-//PETICIONES GET
-router.get('/inicio',index) //PINTA LA PAGINA GENERAL
-router.get('/login', formLogin) //PINTA EL FORMULARIO DE LOGIN
-router.get('/home', userHome) //PINTA EL INICIO DE USUARIOS
-router.get('/admin-home', adminHome) //PINTA EL INICIO DE ADMIN JUNTO AL DASHBOARD
-router.get('/logout', logout) //CIERRA SESION BORRANDO LA COOKIE
-router.get('/login/confirmar-cuenta/:token', confirmAccount)
-router.get('/login/cambiar-contrase%C3%B1a', formPasswordRecovery)
-router.get('/login/actualizar-contrase%C3%B1a/:token', formPasswordUpdate)
+// Definición de rutas para peticiones POST
+router.post('/login', authenticateUser); // VALIDA EL LOGIN
+router.post('/login/cambiar-contrase%C3%B1a', emailChangePassword);
+router.post('/login/actualizar-contrase%C3%B1a/:token', updatePassword);
 
-router.post('/login', authenticateUser) //VALIDA EL LOGIN
-router.post('/login/cambiar-contrase%C3%B1a', emailChangePassword)
-router.post('/login/actualizar-contrase%C3%B1a/:token',updatePassword)
-
-
-
-
-export default router
+export default router;
