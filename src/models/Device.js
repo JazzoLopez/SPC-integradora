8e65ee6d29f0bedb966e722cf198367d76a45630
LuisitoGamer69 <luisivmaraz03@gmail.com>
2023-12-11 00:01:57 -0600

    Agregando dispositivos a la bd

--- src/models/Device.js
+++ src/models/Device.js
@@ -1,25 +1,27 @@
+// Importación del objeto 'DataTypes' y de la instancia de la base de datos ('db')
 import { DataTypes } from "sequelize";
-import db from '../configs/db.js'
+import db from '../configs/db.js';
 
-//tbb_devices
-const Device = db.define('tbb_devices',{
-    typeDevice:{
-        type:DataTypes.ENUM('Impresora','Laptop','Fotocopiadora','Multifuncional','Desktop'),
-        allowNull:false
+// Definición del modelo 'Device' para la tabla 'tbb_devices'
+const Device = db.define('tbb_devices', {
+    // Definición de columnas y sus tipos de datos
+    typeDevice: {
+        type: DataTypes.ENUM('Impresora', 'Laptop', 'Fotocopiadora', 'Multifuncional', 'Desktop'),
+        allowNull: false
     },
-    brand:{
+    brand: {
         type: DataTypes.STRING,
-        allowNull:false
+        allowNull: false
     },
-    model:{
-        type:DataTypes.STRING,
-        allowNull:false
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    serialNumber:{
-        type:DataTypes.STRING,
-        allowNull:false
+    serialNumber: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-
 });
 
+// Exportación del modelo 'Device' para ser utilizado en otras partes de la aplicación
 export default Device;
