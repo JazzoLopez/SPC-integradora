bab36f269f773c8570512815a8b0099a391e96f5
LuisitoGamer69 <luisivmaraz03@gmail.com>
2023-12-11 02:27:05 -0600

    Documentacion en pagination.js

--- src/public/js/pagination.js
+++ src/public/js/pagination.js
@@ -1,27 +1,30 @@
-  $(document).ready(function() {
-    $('#myTable').DataTable({
+// Espera a que el documento HTML esté completamente cargado
+$(document).ready(function() {
+  // Inicializa la tabla con el nombre de ID 'myTable' utilizando la biblioteca DataTables
+  $('#myTable').DataTable({
+      // Configuración del idioma y mensajes personalizados para la tabla
       language: {
-        "sProcessing": "Procesando...",
-        "sLengthMenu": "Mostrar _MENU_ registros por página",
-        "sZeroRecords": "No se encontraron resultados",
-        "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ registros",
-        "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",
-        "sInfoFiltered": "(filtrado de _MAX_ registros en total)",
-        "sInfoPostFix": "",
-        "sSearch": "Buscar:",
-        "sUrl": "",
-        "sInfoThousands": ",",
-        "sLoadingRecords": "Cargando...",
-        "oPaginate": {
-          "sFirst": "Primero",
-          "sLast": "Último",
-          "sNext": "Siguiente",
-          "sPrevious": "Anterior"
-        },
-        "oAria": {
-          "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-        }
+          "sProcessing": "Procesando...",  // Mensaje mostrado durante el procesamiento de la tabla
+          "sLengthMenu": "Mostrar _MENU_ registros por página",  // Menú para seleccionar la cantidad de registros por página
+          "sZeroRecords": "No se encontraron resultados",  // Mensaje mostrado cuando no hay resultados
+          "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ registros",  // Información sobre la cantidad de registros mostrados
+          "sInfoEmpty": "Mostrando 0 a 0 de 0 registros",  // Mensaje cuando no hay registros para mostrar
+          "sInfoFiltered": "(filtrado de _MAX_ registros en total)",  // Información sobre registros filtrados
+          "sInfoPostFix": "",
+          "sSearch": "Buscar:",  // Etiqueta del campo de búsqueda
+          "sUrl": "",
+          "sInfoThousands": ",",  // Separador de miles en la información de registros
+          "sLoadingRecords": "Cargando...",  // Mensaje mostrado durante la carga de registros
+          "oPaginate": {
+              "sFirst": "Primero",  // Etiqueta del botón para ir a la primera página
+              "sLast": "Último",  // Etiqueta del botón para ir a la última página
+              "sNext": "Siguiente",  // Etiqueta del botón para ir a la siguiente página
+              "sPrevious": "Anterior"  // Etiqueta del botón para ir a la página anterior
+          },
+          "oAria": {
+              "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+              "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+          }
       }
-    });
   });
+});
